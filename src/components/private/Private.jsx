dbc629b229c2187145f78fdaaf605b03550e9558
MrRahul <99179217+MrRahul2003@users.noreply.github.com>
2023-05-22 12:04:54 +0530

    Update Private.jsx

--- src/components/private/Private.jsx
+++ src/components/private/Private.jsx
@@ -1,6 +1,6 @@
 // backend url
 
-const backendURL = "http://localhost:5000";
-// const backendURL = "https://crm-ycfg.onrender.com";
+// const backendURL = "http://localhost:5000";
+const backendURL = "https://crm-ycfg.onrender.com";
 
 export { backendURL };
